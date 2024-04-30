57baacac94790bee97d1d4e633f114af74fbf816
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-04-30 18:08:28 +0900

    [Silver III] Title: 이친수, Time: 220 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        long[] dp = new long[N+1];
+
+        if (N>=1) dp[1] = 1;
+        if (N>=2) dp[2] = 1;
+        for (int i=3; i<=N; i++) dp[i] = dp[i-1] + dp[i-2];
+        System.out.println(dp[N]);
+    }
+}
