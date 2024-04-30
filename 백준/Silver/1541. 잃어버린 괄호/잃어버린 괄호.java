10dec558697c292ffd3e531f07757290892d0169
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-04-30 18:02:54 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+ 
+		int sum = Integer.MAX_VALUE;
+		String[] str = in.nextLine().split("-");
+		
+		for(int i = 0; i < str.length; i++) {
+			int temp = 0;
+			String[] add = str[i].split("\\+");
+			
+			for(int j = 0; j < add.length; j++) 
+                temp += Integer.parseInt(add[j]);
+			
+			if (sum == Integer.MAX_VALUE) sum = temp; 
+            else sum -= temp;
+		}
+		System.out.println(sum);
+	}
+}
