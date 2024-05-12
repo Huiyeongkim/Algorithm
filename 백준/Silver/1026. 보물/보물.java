a04b446a278c787a443ce53d665bb4f87e2baa01
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-05-12 22:17:53 +0900

    Update 보물.java

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -4,35 +4,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // N 입력 받기
         int N = scanner.nextInt();
-
-        // A 배열 입력 받기
-        int[] A = new int[N];
+        
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            A[i] = scanner.nextInt();
+            arr[i] = scanner.nextInt();
         }
 
-        // B 배열 입력 받기
-        int[] B = new int[N];
+        int[] arr2 = new int[N];
         for (int i = 0; i < N; i++) {
-            B[i] = scanner.nextInt();
+            arr2[i] = scanner.nextInt();
         }
+        
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
 
-        // A 배열 오름차순으로 정렬
-        Arrays.sort(A);
-
-        // B 배열 내림차순으로 정렬
-        Arrays.sort(B);
-
-        // S 값 계산
         int S = 0;
         for (int i = 0; i < N; i++) {
-            S += A[i] * B[N - 1 - i];
+            S += arr[i] * arr2[N - 1 - i];
         }
-
-        // S의 최솟값 출력
-         System.out.println(S);
+        System.out.println(S);
     }
 }
