d4659e362f85b3396fc52cd16ee6cbbb351c70a5
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-05-13 19:41:16 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int k = 1;
+        int i = 1;
+        while(true) {
+            k *= i;
+            if (n==k) {
+                answer = i;
+                break;
+            }
+            else if (n<k) {
+                answer = i-1;
+                break;
+            }
+            if (n>k)
+            i++;
+        }
+        return answer;
+    }
+}
