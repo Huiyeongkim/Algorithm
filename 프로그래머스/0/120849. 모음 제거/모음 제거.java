17c4dfff4de4bf9b1bb6d485bfaea04e0f413b58
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-05-13 19:52:52 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,12 +1,7 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        for (int i=0; i<my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            
-            if (!(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'))
-                answer += my_string.charAt(i);
-        }
+        answer = my_string.replaceAll("[aeiou]","");
         return answer;
     }
 }
