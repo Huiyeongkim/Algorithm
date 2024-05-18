51e330d856179664f8f0569d4b023c569b97cbaf
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-05-18 22:05:36 +0900

    [Silver I] Title: 회의실 배정, Time: 528 ms, Memory: 44228 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,27 +1,31 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int count = 0;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int[][] time = new int[N][2];
-        for (int i=0; i<N; i++) {
-            time[i][0] = in.nextInt();
-            time[i][1] = in.nextInt();
+        StringTokenizer st ;
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(time, (o1, o2) -> {
+
+        Arrays.sort(arr, (o1, o2) -> {
             if (o1[1]==o2[1]) return o1[0]-o2[0];
             return o1[1]-o2[1];
         });
 
-        int endTime = 0;
+        int end = 0;
+        int count = 0;
         for (int i=0; i<N; i++) {
-            if (endTime <= time[i][0]) {
-                endTime = time[i][1];
+            if (end <= arr[i][0]) {
+                end = arr[i][1];
                 count++;
             }
         }
