b36210111666f3479056ee572a1ba27a878bbc24
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-05-19 02:02:58 +0900

    [Gold V] Title: 강의실 배정, Time: 672 ms, Memory: 72500 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,29 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int count = 0;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int[][] meeting = new int[N][2];
-        for (int i=0; i<N; i++) {
-            meeting[i][0] = in.nextInt();
-            meeting[i][1] = in.nextInt();
+        StringTokenizer st;
+        long[][] arr = new long[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Long.parseLong(st.nextToken());
+            arr[i][1] = Long.parseLong(st.nextToken());
         }
-        Arrays.sort(meeting, (o1, o2) -> {
-            if (o1[0]==o2[0]) return o1[1]-o2[1];
-            return o1[0]-o2[0];
-        });
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        for (int i=0; i<N; i++) {
-            int end = meeting[i][1];
-            if (!priorityQueue.isEmpty()&&priorityQueue.peek()<=meeting[i][0]) priorityQueue.poll();
-            priorityQueue.add(end);
+        Arrays.sort(arr, Comparator.comparingLong(o -> o[0]));
+
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            long start = arr[i][0];
+            long end = arr[i][1];
+            if (!pq.isEmpty() && pq.peek() <= start) {
+                pq.poll();
+            }
+            pq.offer(end);
         }
-        System.out.println(priorityQueue.size());
+        System.out.println(pq.size());
     }
 }
