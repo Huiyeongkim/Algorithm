842ef38f1123af0d156f5830a3af0c1dc1ad7504
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-05-24 11:42:58 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.20 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,20 +1,9 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        int k = 0;
-
-        for (int i=0; i<my_string.length(); i++) {
-            char c = my_string.charAt(i);
-            if (c>='0'&&c<='9')  {
-                k = c-'0' + k*10;   
-                if (i == my_string.length() - 1) {
-                    answer += k;
-                }
-            }
-            else {
-                answer += k;
-                k = 0;
-            }
+        String[] arr = my_string.replaceAll("[a-zA-Z]"," ").split(" ");
+        for (int i=0; i<arr.length; i++) {
+            if (!arr[i].equals("")) answer+= Integer.parseInt(arr[i]);
         }
         return answer;
     }
