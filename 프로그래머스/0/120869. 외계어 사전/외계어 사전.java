52c146890ab2b4b3ff2cc21233beece51065e8fe
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-05-24 23:38:32 +0900

    [level 0] Title: 외계어 사전, Time: 3.06 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        int answer = 2;
+        for (int i=0; i<dic.length; i++) {
+            int count = 0;
+            for (int j=0; j<spell.length; j++) {
+                if (dic[i].contains(spell[j])) {
+                    count++;
+                    dic[i].replace(spell[j], "");
+                    System.out.println("dic = " + dic[i]);
+                }
+            }
+            if (count==spell.length) answer = 1;
+        }
+        return answer;
+    }
+}
