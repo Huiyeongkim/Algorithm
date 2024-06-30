7628aa47da413d30dd69300dd926fdef809e1210
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2024-06-30 16:39:37 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static final int MOD = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        long absN = Math.abs(n);
+        long fib = Fibonacci(absN);
+
+        if (n < 0 && absN % 2 == 0) {
+            fib = -fib;
+        }
+
+        // 출력
+        if (fib > 0) {
+            System.out.println(1);
+        } else if (fib == 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(-1);
+        }
+
+        System.out.println(Math.abs(fib));
+    }
+
+    static long Fibonacci(long n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        long a = 0;
+        long b = 1;
+
+        for (long i = 2; i <= n; i++) {
+            long sum = (a + b)% MOD;
+            a = b;
+            b = sum;
+        }
+
+        return b;
+    }
+}
