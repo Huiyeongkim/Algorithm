a78accf8f97ca76f7ba4da7b2c1b511ade3655f6
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2025-07-02 01:41:44 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 776 ms, Memory: 298324 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -7,8 +7,8 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
+        int N = scanner.nextInt(); // 총 인원 수
+        int K = scanner.nextInt(); // 제거할 사람의 간격
 
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
@@ -16,14 +16,15 @@ public class Main {
         }
 
         List<Integer> arr = new ArrayList<>();
+
         while (!queue.isEmpty()) {
             for (int i = 0; i < K - 1; i++) {
                 int person = queue.poll();
                 queue.add(person); 
             }
+
            arr.add(queue.poll());
         }
-        
         System.out.print("<");
         for (int i = 0; i < arr.size(); i++) {
             System.out.print(arr.get(i));
