65586987c01e09305bd2b4586833b0228a403e94
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2025-10-15 18:46:12 +0900

    [Silver IV] Title: 2+1 세일, Time: 388 ms, Memory: 26812 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -1,21 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int sum  = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
         Integer[] arr = new Integer[N];
-
-        for (int i=0; i<N; i++) {
-            arr[i] = in.nextInt();
-        }
         
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
         Arrays.sort(arr, Comparator.reverseOrder());
-        for (int i=0; i<N; i++) {
-            if (i%3==2) continue;
+        for (int i = 0; i < N; i++) {
+            if (i % 3 == 2) continue;
             sum += arr[i];
         }
 
