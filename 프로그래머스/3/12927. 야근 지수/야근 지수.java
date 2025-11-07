e6cb2c5cc5d63189b98baad8895031ebb77a7057
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2025-11-07 15:11:25 +0900

    [level 3] Title: 야근 지수, Time: 213.34 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] works) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int w : works) {
+            pq.offer(w);
+        }
+
+        while (n > 0 && !pq.isEmpty()) {
+            int max = pq.poll();
+            if (max == 0) break;
+            pq.offer(max - 1);
+            n--;
+        }
+
+        long answer = 0;
+        while (!pq.isEmpty()) {
+            long w = pq.poll();
+            answer += w * w;
+        }
+
+        return answer;
+    }
+}
