d19ad4ecd3d1d1df1210ea56c56b2a033fc1716c
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2026-02-11 23:21:27 +0900

    [level 3] Title: 단속카메라, Time: 6.99 ms, Memory: 58.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        int answer = 1;
+        int max = routes[0][1];
+        for (int i=1; i<routes.length; i++) {
+            if (max < routes[i][0]) {
+                answer++;
+                max = routes[i][1];
+            }
+        }
+        return answer;
+    }
+}
