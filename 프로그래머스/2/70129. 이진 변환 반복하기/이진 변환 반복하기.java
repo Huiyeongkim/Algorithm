8b61a2db2915ca1963e53534cc7026ad9eb900f0
Huiyeongkim <146138986+Huiyeongkim@users.noreply.github.com>
2026-02-23 23:32:43 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 12.89 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -3,23 +3,24 @@ class Solution {
         int[] answer = new int[2];
         int count = 0;
         int conversion = 0;
-        while(!s.equals("1")) {
+        while(true) {
+            if (s.equals("1")) break;
             conversion++;
-            int originalLength = s.length();
-            s = s.replace("0", "");
-            int newLength = s.length();
-            count += (originalLength - newLength);
+            int original = s.length();
+            s = s.replace("0","");
+            int length = s.length();
+            count+=(original-length);
             s = "";
-            while(newLength>=1) {
-                if (newLength%2==0) {
+            while(length>=1) {
+                if (length%2==0) {
                     s += "0";
                 }
                 else {
                     s += "1";
                 }
-                newLength /= 2;
+                length /= 2;
             }
-        }
+        }   
         answer[0] = conversion;
         answer[1] = count;
         
